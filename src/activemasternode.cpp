1ab22aa010124f36c32bf2ad692e636a87384715
Dud-man <49316658+Dud-man@users.noreply.github.com>
2019-08-07 21:07:33 +0200

    [2.0] Collateral and Protocol Update

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -8,7 +8,7 @@
 #include "spork.h"
 
 //
-// Bootup the Masternode, look for a 10000 Mero input and register on the network
+// Bootup the Masternode, look for a 5000 Mero input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
