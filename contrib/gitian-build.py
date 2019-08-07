7922308fbc94ab0750d3d33696d71e145a4f2102
Dud-man <49316658+Dud-man@users.noreply.github.com>
2019-08-07 21:15:18 +0200

    [Build] Update 1

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -29,7 +29,7 @@ def setup():
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('mero'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/MeroCoin/mero.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/Dud-man/mero'])
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
