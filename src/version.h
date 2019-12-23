db3b088c416235d7c8a37d23c521a0afdecdc88e
Dud-man <49316658+Dud-man@users.noreply.github.com>
2019-12-23 18:07:57 +0200

    2.0 Update(#4)

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 //! only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 70909;
+static const int NOBLKS_VERSION_END = 70812;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
